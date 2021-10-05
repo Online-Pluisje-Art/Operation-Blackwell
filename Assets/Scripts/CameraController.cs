be218079202d5c973aa4d22b11cd0664690f2601
Noah Knegt <github@noahknegt.com>
2021-10-05 11:02:09 +0200

    core: Fixup all the compilation errors
    Signed-off-by: Noah Knegt <github@noahknegt.com>

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,25 +1,27 @@
 using UnityEngine;
 
-class CameraController : MonoBehaviour {
-	// The movementcontroller of the camera.
-	private ICameraMovementController cameraMovementController_;
+namespace OperationBlackwell {
+	class CameraController : MonoBehaviour {
+		// The movementcontroller of the camera.
+		private ICameraMovementController cameraMovementController_;
 
-	/*
-	 * Initializes the camera controller
-	 * and sets the camera in the movement controller.
-	 */
-	private void Awake() {
-		Camera camera = GetComponent<Camera>();
-		cameraMovementController_ = new CameraMovement();
-		cameraMovementController_.SetCamera(camera);
-	}
+		/*
+		* Initializes the camera controller
+		* and sets the camera in the movement controller.
+		*/
+		private void Awake() {
+			Camera camera = GetComponent<Camera>();
+			cameraMovementController_ = new CameraMovement();
+			cameraMovementController_.SetCamera(camera);
+		}
 
 
-	/*
-	 * Updates the camera movement
-	 * and moves the camera.
-	 */
-	private void Update() {
-		cameraMovementController_.HandleInput();
+		/*
+		* Updates the camera movement
+		* and moves the camera.
+		*/
+		private void Update() {
+			cameraMovementController_.HandleInput();
+		}
 	}
 }
