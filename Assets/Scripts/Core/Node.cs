0c635c9352bde952032cf6d6dc42c78e2318ffd5
Dennis Bonke <admin@dennisbonke.com>
2021-10-08 21:55:16 +0200

    core: Change how the grid is generated and fix the movement logic.
    This should be good enough for the tech demo.
    
    Signed-off-by: Dennis Bonke <admin@dennisbonke.com>

--- Assets/Scripts/Core/Node.cs
+++ Assets/Scripts/Core/Node.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 namespace OperationBlackwell.Core {
 	public class Node {
-		public bool walkable { get; private set; }
+		public enum CoverStatus {
+			NONE,
+			HALF,
+			FULL,
+		}
+		// Holds the amount of cover this tile gives.
+		public CoverStatus cover {get; protected set;}
+		// Holds if the tile can be walked over.
+		public bool walkable {get; protected set;}
+		// Holds if the tile can be shot through.
+		public bool shootable {get; protected set;}
 		public Vector3 worldPosition { get; private set; }
 		public int gridX { get; private set; }
 		public int gridY { get; private set; }
+
+		private Grid<Node> grid_;
 		// public int gCost;
 		// public int hCost;
 
@@ -19,11 +31,14 @@ namespace OperationBlackwell.Core {
 
 		// public Node parent;
 
-		public Node(bool walkable, Vector3 worldPosition, int gridX, int gridY) {
-			this.walkable = walkable;
+		public Node(Vector3 worldPosition, int gridX, int gridY, Grid<Node> grid, bool walkable, bool shootable, CoverStatus cover) {
 			this.worldPosition = worldPosition;
 			this.gridX = gridX;
 			this.gridY = gridY;
+			this.grid_ = grid;
+			this.walkable = walkable;
+			this.shootable = shootable;
+			this.cover = cover;
 		}
 	}
 }
--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -5,107 +5,216 @@ using UnityEngine;
 namespace OperationBlackwell {
 	public class MovementController : MonoBehaviour {
 
-		private const bool DebugMovement = false;
-		private Grid grid;
-		[SerializeField] private Transform player;
+		private const bool DebugMovement = true;
+		// private Grid grid;
+		// [SerializeField] private Transform player;
 
 		// private void Start() {
 		// 	// This creates a grid of 6 wide and 3 high, cell size 10 and no offset (so bottom left is 0,0,0).
 		// 	grid = new Grid(6, 3, 10f, new Vector3(0, 0, 0));
 		// }
 
-		private void Update() {
-			if(Input.GetMouseButtonDown(0)) {
-				Vector3 gridClicked = GetMouseWorldPosition3d();
-				if(gridClicked == Vector3.zero) {
-					if(DebugMovement) {
-						// Chances of you clicking on _exactly_ 0,0,0 are almost 0, so oob is likely.
-						Debug.Log("Clicked at 0,0,0 or out of the map!");
-					}
-				} else {
-					// Get the current position and the position clicked in X, Z coordinates.
-					int currentX, currentZ, clickedX, clickedZ, diffX, diffZ;
-					Vector3 currentPosition = player.position;
-					grid.GetXZ(currentPosition, out currentX, out currentZ);
-					grid.GetXZ(gridClicked, out clickedX, out clickedZ);
-					if(DebugMovement) {
-						Debug.Log("Player current X: " + currentX + ", current Z: " + currentZ);
-						Debug.Log("Player clicked X: " + clickedX + ", clicked Z: " + clickedZ);
-					}
-					// Check if we can move, if we can, do so.
-					if(CanMove(currentX, currentZ, clickedX, clickedZ, out diffX, out diffZ)) {
-						if(DebugMovement) {
-							Debug.Log("Yes we can move");
-						}
-						/*
-						* Translate our tictac, to make it work as we want we do -diffX and -diffZ.
-						* We multiply by our cellSize to make it not suck and actually move entire cells at once.
-						* Pass 0 as Y, as we don't care about it.
-						*/
-						player.Translate(-diffX * grid.cellSize, 0, -diffZ * grid.cellSize);
-					} else {
-						if(DebugMovement) {
-							Debug.Log("No we can't move");
-						}
-					}
-				}
-			}
-		}
+		// private void Update() {
+		// 	if(Input.GetMouseButtonDown(0)) {
+		// 		Vector3 gridClicked = GetMouseWorldPosition3d();
+		// 		if(gridClicked == Vector3.zero) {
+		// 			if(DebugMovement) {
+		// 				// Chances of you clicking on _exactly_ 0,0,0 are almost 0, so oob is likely.
+		// 				Debug.Log("Clicked at 0,0,0 or out of the map!");
+		// 			}
+		// 		} else {
+		// 			// Get the current position and the position clicked in X, Z coordinates.
+		// 			int currentX, currentZ, clickedX, clickedZ, diffX, diffZ;
+		// 			Vector3 currentPosition = player.position;
+		// 			grid.GetXZ(currentPosition, out currentX, out currentZ);
+		// 			grid.GetXZ(gridClicked, out clickedX, out clickedZ);
+		// 			if(DebugMovement) {
+		// 				Debug.Log("Player current X: " + currentX + ", current Z: " + currentZ);
+		// 				Debug.Log("Player clicked X: " + clickedX + ", clicked Z: " + clickedZ);
+		// 			}
+		// 			// Check if we can move, if we can, do so.
+		// 			if(CanMove(currentX, currentZ, clickedX, clickedZ, out diffX, out diffZ)) {
+		// 				if(DebugMovement) {
+		// 					Debug.Log("Yes we can move");
+		// 				}
+		// 				/*
+		// 				* Translate our tictac, to make it work as we want we do -diffX and -diffZ.
+		// 				* We multiply by our cellSize to make it not suck and actually move entire cells at once.
+		// 				* Pass 0 as Y, as we don't care about it.
+		// 				*/
+		// 				player.Translate(-diffX * grid.cellSize, 0, -diffZ * grid.cellSize);
+		// 			} else {
+		// 				if(DebugMovement) {
+		// 					Debug.Log("No we can't move");
+		// 				}
+		// 			}
+		// 		}
+		// 	}
+		// }
 
-		/*
-		* CodeMonkey code, grab me some 3d mouse position.
-		* Requires a collider below the play grid for our use case!
-		* TODO: This collider grid is hacked in now, make this nicer.
-		*/
-		private Vector3 GetMouseWorldPosition3d() {
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			if(Physics.Raycast(ray, out RaycastHit raycastHit, 999f)) {
-				return raycastHit.point;
-			} else {
-				return Vector3.zero;
-			}
-		}
+		// /*
+		// * CodeMonkey code, grab me some 3d mouse position.
+		// * Requires a collider below the play grid for our use case!
+		// * TODO: This collider grid is hacked in now, make this nicer.
+		// */
+		// private Vector3 GetMouseWorldPosition3d() {
+		// 	Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		// 	if(Physics.Raycast(ray, out RaycastHit raycastHit, 999f)) {
+		// 		return raycastHit.point;
+		// 	} else {
+		// 		return Vector3.zero;
+		// 	}
+		// }
 
-		/*
-		* Checks if the clicked coordinate is valid, returning true if it is.
-		* We need diffX and diffZ to move, so for now they are out parameters.
-		*/
-		private bool CanMove(int currentX, int currentZ, int clickedX, int clickedZ, out int diffX, out int diffZ) {
-			// Move can happen unless exited out early.
-			bool ret = true;
-			// Unusable, but needs setting for early bail.
-			diffX = diffZ = -999;
-			// We're already here! Why move?
-			if(currentX == clickedX && currentZ == clickedZ) {
-				return false;
-			}
-			// Out of range, we won't move!
-			if(clickedX < 0 || clickedZ < 0 || clickedX > grid.width || clickedZ > grid.height) {
-				return false;
-			}
-			/*
-			* Calculate the actual diff, a negative Z is up, positive Z is down.
-			* Negative X is to the right, positive X is to the left, yes confusing.
-			* Catch invalid movements (so either X or Z diff bigger than 1 or minus 1).
-			*/
-			diffX = currentX - clickedX;
-			diffZ = currentZ - clickedZ;
-			if(DebugMovement) {
-				Debug.Log("diffX: " + diffX + ", diffZ: " + diffZ);
-			}
-			if(diffX < -1 || diffX > 1) {
-				if(DebugMovement) {
-					Debug.Log("Can't move on invalid X (diffX: " + diffX + ")!");
-				}
-				ret = false;
-			}
-			if(diffZ < -1 || diffZ > 1) {
-				if(DebugMovement) {
-					Debug.Log("Can't move on invalid Z (diffZ: " + diffZ + ")!");
-				}
-				ret = false;
-			}
-			return ret;
-		}
+		// /*
+		// * Checks if the clicked coordinate is valid, returning true if it is.
+		// * We need diffX and diffZ to move, so for now they are out parameters.
+		// */
+		// private bool CanMove(int currentX, int currentZ, int clickedX, int clickedZ, out int diffX, out int diffZ) {
+		// 	// Move can happen unless exited out early.
+		// 	bool ret = true;
+		// 	// Unusable, but needs setting for early bail.
+		// 	diffX = diffZ = -999;
+		// 	// We're already here! Why move?
+		// 	if(currentX == clickedX && currentZ == clickedZ) {
+		// 		return false;
+		// 	}
+		// 	// Out of range, we won't move!
+		// 	if(clickedX < 0 || clickedZ < 0 || clickedX > grid.width || clickedZ > grid.height) {
+		// 		return false;
+		// 	}
+		// 	/*
+		// 	* Calculate the actual diff, a negative Z is up, positive Z is down.
+		// 	* Negative X is to the right, positive X is to the left, yes confusing.
+		// 	* Catch invalid movements (so either X or Z diff bigger than 1 or minus 1).
+		// 	*/
+		// 	diffX = currentX - clickedX;
+		// 	diffZ = currentZ - clickedZ;
+		// 	if(DebugMovement) {
+		// 		Debug.Log("diffX: " + diffX + ", diffZ: " + diffZ);
+		// 	}
+		// 	if(diffX < -1 || diffX > 1) {
+		// 		if(DebugMovement) {
+		// 			Debug.Log("Can't move on invalid X (diffX: " + diffX + ")!");
+		// 		}
+		// 		ret = false;
+		// 	}
+		// 	if(diffZ < -1 || diffZ > 1) {
+		// 		if(DebugMovement) {
+		// 			Debug.Log("Can't move on invalid Z (diffZ: " + diffZ + ")!");
+		// 		}
+		// 		ret = false;
+		// 	}
+		// 	return ret;
+		// }
+
+	// 	private void Update() {
+	// 		if(Input.GetMouseButtonDown(0)) {
+	// 			Vector3 gridClicked = Utils.GetMouseWorldPosition3d();
+	// 			if(gridClicked == Vector3.zero) {
+	// 				if(DebugMovement) {
+	// 					// Chances of you clicking on _exactly_ 0,0,0 are almost 0, so oob is likely.
+	// 					Debug.Log("Clicked at 0,0,0 or out of the map!");
+	// 				}
+	// 			} else {
+	// 				// Get the current position and the position clicked in X, Z coordinates.
+	// 				int currentX, currentZ, clickedX, clickedZ, diffX, diffZ;
+	// 				Vector3 currentPosition = player_.position;
+	// 				Vector3 movement;
+	// 				grid_.GetXZ(currentPosition, out currentX, out currentZ);
+	// 				grid_.GetXZ(gridClicked, out clickedX, out clickedZ);
+	// 				if(DebugMovement) {
+	// 					Debug.Log("Player current X: " + currentX + ", current Z: " + currentZ);
+	// 					Debug.Log("Player clicked X: " + clickedX + ", clicked Z: " + clickedZ);
+	// 				}
+	// 				// Check if we can move, if we can, do so.
+	// 				// if(CanMove(currentX, currentZ, clickedX, clickedZ, out diffX, out diffZ)) {
+	// 				if(CanMove(currentPosition, gridClicked, currentX, currentZ, clickedX, clickedZ, out movement)) {
+	// 					if(DebugMovement) {
+	// 						Debug.Log("Yes we can move");
+	// 					}
+	// 					/*
+	// 					* Translate our tictac, to make it work as we want we do -diffX and -diffZ.
+	// 					* We multiply by our cellSize to make it not suck and actually move entire cells at once.
+	// 					* Pass 0 as Y, as we don't care about it.
+	// 					*/
+	// 					// player_.Translate(-diffX * grid_.cellSize, 0, -diffZ * grid_.cellSize);
+	// 					player_.Translate(-movement);
+	// 				} else {
+	// 					if(DebugMovement) {
+	// 						Debug.Log("No we can't move");
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+
+	// 	/*
+	// 	* Checks if the clicked coordinate is valid, returning true if it is.
+	// 	* We need diffX and diffZ to move, so for now they are out parameters.
+	// 	*/
+	// 	private bool CanMove(Vector3 playerPosition, Vector3 targetPosition, int currentX, int currentZ, int clickedX, int clickedZ, out Vector3 movement) {// out int diffX, out int diffZ) {
+	// 		// Move can happen unless exited out early.
+	// 		movement = Vector3.zero;
+	// 		// bool ret = true;
+	// 		// Unusable, but needs setting for early bail.
+	// 		// diffX = diffZ = -999;
+	// 		// We're already here! Why move?
+	// 		if(currentX == clickedX && currentZ == clickedZ) {
+	// 			return false;
+	// 		}
+	// 		// Out of range, we won't move!
+	// 		// if(clickedX > grid_.gridSizeX / 2 || clickedZ > grid_.gridSizeY / 2 || clickedX < -grid_.gridSizeX / 2 || clickedZ < -grid_.gridSizeY / 2) {
+	// 		// 	return false;
+	// 		// }
+	// 		if(clickedX < 0 || clickedZ < 0 || clickedX > grid_.gridSizeX || clickedZ > grid_.gridSizeY) {
+	// 			return false;
+	// 		}
+
+	// 		Node playerNode = grid_.NodeFromWorldPoint(playerPosition);
+	// 		Node targetNode = grid_.NodeFromWorldPoint(targetPosition);
+
+	// 		if(playerNode.worldPosition == targetNode.worldPosition) {
+	// 			Debug.Log("Check 2");
+	// 			return false;
+	// 		}
+	// 		// We're moving, so we need to check if we can move.
+	// 		List<Node> neighbours = grid_.GetNeighbours(playerNode);
+	// 		if(neighbours.Contains(targetNode)) {
+	// 			// TODO: Check if node is walkable!
+	// 			if(targetNode.walkable) {
+	// 				movement = playerNode.worldPosition - targetNode.worldPosition;
+	// 				return true;	
+	// 			} else {
+	// 				return false;
+	// 			}
+	// 		} else {
+	// 			Debug.Log("Check 3");
+	// 			return false;
+	// 		}
+	// 		/*
+	// 		* Calculate the actual diff, a negative Z is up, positive Z is down.
+	// 		* Negative X is to the right, positive X is to the left, yes confusing.
+	// 		* Catch invalid movements (so either X or Z diff bigger than 1 or minus 1).
+	// 		*/
+	// 		// diffX = currentX - clickedX;
+	// 		// diffZ = currentZ - clickedZ;
+	// 		// if(DebugMovement) {
+	// 		// 	Debug.Log("diffX: " + diffX + ", diffZ: " + diffZ);
+	// 		// }
+	// 		// if(diffX < -1 || diffX > 1) {
+	// 		// 	if(DebugMovement) {
+	// 		// 		Debug.Log("Can't move on invalid X (diffX: " + diffX + ")!");
+	// 		// 	}
+	// 		// 	ret = false;
+	// 		// }
+	// 		// if(diffZ < -1 || diffZ > 1) {
+	// 		// 	if(DebugMovement) {
+	// 		// 		Debug.Log("Can't move on invalid Z (diffZ: " + diffZ + ")!");
+	// 		// 	}
+	// 		// 	ret = false;
+	// 		// }
+	// 		// return ret;
+	// 	}
 	}
 }
