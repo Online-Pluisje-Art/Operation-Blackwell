07d336c661f93d5e0582c1ae911ddde49a1d110f
Noah Knegt <github@noahknegt.com>
2021-10-14 15:20:21 +0200

    core: Add unity 2d project
    Signed-off-by: Noah Knegt <github@noahknegt.com>

---
+++ Assets/Tests/PlayMode/PlayTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class PlayTest {
+	// A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+	// `yield return null;` to skip a frame.
+	[UnityTest]
+	public IEnumerator PlayTestWithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+		// Use yield to skip a frame.
+		yield return null;
+	}
+}
