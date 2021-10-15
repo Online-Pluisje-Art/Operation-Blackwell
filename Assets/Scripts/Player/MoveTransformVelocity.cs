59a555c4291bd86a3d8ca262e03aff9aafadda44
Noah Knegt <github@noahknegt.com>
2021-10-15 19:24:07 +0200

    player: Add a modular character system
    Signed-off-by: Noah Knegt <github@noahknegt.com>

---
+++ Assets/Scripts/Player/MoveTransformVelocity.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OperationBlackwell.Player {
+	public class MoveTransformVelocity : MonoBehaviour, IMoveVelocity {
+		[SerializeField] private float moveSpeed_;
+
+		private Vector3 velocityVector_;
+		private PlayerBase characterBase_;
+
+		private void Awake() {
+			characterBase_ = GetComponent<PlayerBase>();
+		}
+
+		public void SetVelocity(Vector3 velocityVector) {
+			this.velocityVector_ = velocityVector;
+		}
+
+		private void Update() {
+			transform.position += velocityVector_ * moveSpeed_ * Time.deltaTime;
+		}
+
+		public void Disable() {
+			this.enabled = false;
+		}
+
+		public void Enable() {
+			this.enabled = true;
+		}
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMovementMouse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OperationBlackwell.Core;
+
+namespace OperationBlackwell.Player {    
+	public class PlayerMovementMouse : MonoBehaviour {
+		private void Update() {
+			if (Input.GetMouseButtonDown(1)) {
+				GetComponent<IMovePosition>().SetMovePosition(Utils.GetMouseWorldPosition());
+			}
+		}
+	}
+}
