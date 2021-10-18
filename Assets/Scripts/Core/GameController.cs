26979eb6efff84b1f1c61c5992c9fb31cc35dd8a
Dennis Bonke <admin@dennisbonke.com>
2021-10-18 14:53:50 +0200

    core: Split off painting into a painting controller
    Signed-off-by: Dennis Bonke <admin@dennisbonke.com>

--- Assets/Scripts/Core/GameController.cs
+++ Assets/Scripts/Core/GameController.cs
@@ -20,16 +20,17 @@ namespace OperationBlackwell.Core {
 		public delegate void MoveEvent(Vector3 position);
 		public event MoveEvent Moved;
 
-		private Grid<Tilemap.Node> grid_;
+		public Grid<Tilemap.Node> grid {get; private set;}
+
 		public GridPathfinding gridPathfinding { get; private set; }
 		public Tilemap tilemap {get; private set;}
 
 		[SerializeField] private TilemapVisual tilemapVisual_;
 		private Tilemap.Node.NodeSprite nodeSprite_;
 		private void Awake() {
-			grid_ = new Grid<Tilemap.Node>((int)gridWorldSize_.x, (int)gridWorldSize_.y, cellSize_, new Vector3(0, 0, 0), 
+			grid = new Grid<Tilemap.Node>((int)gridWorldSize_.x, (int)gridWorldSize_.y, cellSize_, new Vector3(0, 0, 0), 
 				(Grid<Tilemap.Node> g, Vector3 worldPos, int x, int y) => new Tilemap.Node(worldPos, x, y, g, true, true, false));
-			tilemap = new Tilemap(grid_);
+			tilemap = new Tilemap(grid);
 			Instance = this;
 			Vector3 origin = new Vector3(0, 0);
 
@@ -45,55 +46,15 @@ namespace OperationBlackwell.Core {
 		}
 
 		private void Update() {
-			HandlePainting();
 			HandleMisc();
 		}
 
-		private void HandlePainting() {
-			// Tilemap code, rightclick please!
-			if(Input.GetKeyDown(KeyCode.T)) {
-				nodeSprite_ = Tilemap.Node.NodeSprite.NONE;
-			}
-			if(Input.GetKeyDown(KeyCode.Y)) {
-				nodeSprite_ = Tilemap.Node.NodeSprite.PIT;
-			}
-			if(Input.GetKeyDown(KeyCode.U)) {
-				nodeSprite_ = Tilemap.Node.NodeSprite.FLOOR;
-			}
-			if(Input.GetKeyDown(KeyCode.I)) {
-				nodeSprite_ = Tilemap.Node.NodeSprite.WALL;
-			}
-			if(Input.GetKeyDown(KeyCode.O)) {
-				nodeSprite_ = Tilemap.Node.NodeSprite.COVER;
-			}
-			if(Input.GetMouseButtonDown(1)) {
-				Vector3 mouseWorldPosition = Utils.GetMouseWorldPosition();
-				Tilemap.Node node = grid_.GetGridObject(mouseWorldPosition);
-				node.SetNodeSprite(nodeSprite_);
-				grid_.TriggerGridObjectChanged(node.gridX, node.gridY);
-			}
-		}
-
 		public Grid<Tilemap.Node> GetGrid() {
-			return grid_;
+			return grid;
 		}
 
 		public MovementTilemap GetMovementTilemap() {
 			return movementTilemap_;
-		}
-
-		private void HandleSaveLoad() {
-			if(Input.GetKeyDown(KeyCode.P)) {
-				System.String saveName = getSaveName();
-				tilemap.Save(saveName);
-			}
-			if(Input.GetKeyDown(KeyCode.L)) {
-				tilemap.Load();
-			}
-		}
-
-		private System.String getSaveName() {
-			return "testing";
 		}
 
 		private void HandleMisc() {
---
+++ Assets/Scripts/Core/PaintingController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OperationBlackwell.Core {
+	public class PaintingController : MonoBehaviour {
+
+		private Tilemap.Node.NodeSprite nodeSprite_;
+
+		private void Update() {
+			HandlePainting();
+		}
+
+		private void HandlePainting() {
+			// Tilemap code, rightclick please!
+			if(Input.GetKeyDown(KeyCode.T)) {
+				nodeSprite_ = Tilemap.Node.NodeSprite.NONE;
+			}
+			if(Input.GetKeyDown(KeyCode.Y)) {
+				nodeSprite_ = Tilemap.Node.NodeSprite.PIT;
+			}
+			if(Input.GetKeyDown(KeyCode.U)) {
+				nodeSprite_ = Tilemap.Node.NodeSprite.FLOOR;
+			}
+			if(Input.GetKeyDown(KeyCode.I)) {
+				nodeSprite_ = Tilemap.Node.NodeSprite.WALL;
+			}
+			if(Input.GetKeyDown(KeyCode.O)) {
+				nodeSprite_ = Tilemap.Node.NodeSprite.COVER;
+			}
+			if(Input.GetMouseButtonDown(1)) {
+				Vector3 mouseWorldPosition = Utils.GetMouseWorldPosition();
+				Tilemap.Node node = GameController.Instance.grid.NodeFromWorldPoint(mouseWorldPosition);
+				node.SetNodeSprite(nodeSprite_);
+				GameController.Instance.grid.TriggerGridObjectChanged(node.gridX, node.gridY);
+			}
+		}
+
+	}
+}
