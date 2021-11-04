75c63a129b96b96fda6a342079855638dfc8e294
Noah Knegt <github@noahknegt.com>
2021-11-04 10:48:05 +0100

    UI: Update the HUD on a event
    Signed-off-by: Noah Knegt <github@noahknegt.com>

--- Assets/Scripts/UI/HUDController.cs
+++ Assets/Scripts/UI/HUDController.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using OperationBlackwell.Core;
 
 namespace OperationBlackwell.UI {
 	public class HUDController : MonoBehaviour {
 
 		[SerializeField] private Text ActionPointsText_;
 
-		private void Update() {
-			ActionPointsText_.text = "Action Points: " + OperationBlackwell.Core.GridCombatSystem.Instance.GetActiveUnit().GetActionPoints();
+		private void Awake() {
+			GridCombatSystem.Instance.OnUnitActionPointsChanged += UpdateActionPoints;
 		}
 
+		private void UpdateActionPoints(object sender, GridCombatSystem.UnitEvent args) {
+			ActionPointsText_.text = "Action Points: " + args.unit.GetActionPoints().ToString();
+		}
 	}
 }
