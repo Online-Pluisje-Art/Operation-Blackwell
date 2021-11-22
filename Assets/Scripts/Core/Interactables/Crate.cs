13cc37c6c9804b04585a3075a6f3bac85828b7fe
Noah Knegt <github@noahknegt.com>
2021-11-22 12:34:33 +0100

    style: Fix minor styling issues
    Signed-off-by: Noah Knegt <github@noahknegt.com>

--- Assets/Scripts/Core/Interactables/Crate.cs
+++ Assets/Scripts/Core/Interactables/Crate.cs
@@ -2,25 +2,25 @@ using UnityEngine;
 using System.Collections;
 
 namespace OperationBlackwell.Core {
-    public class Crate : MonoBehaviour, IInteractable {
-        [SerializeField] private int range_;
-        [SerializeField] private int cost_;
-        private void Start() {
-            GameController.Instance.GetGrid().GetGridObject(transform.position).SetInteractable(this);
-        }
+	public class Crate : MonoBehaviour, IInteractable {
+		[SerializeField] private int range_;
+		[SerializeField] private int cost_;
+		private void Start() {
+			GameController.Instance.GetGrid().GetGridObject(transform.position).SetInteractable(this);
+		}
 
-        public void Interact() {
-            Debug.Log("Interacting with crate");
-        }
+		public void Interact() {
+			Debug.Log("Interacting with crate");
+		}
 
-        public bool IsInRange(CoreUnit unit) {
-            // Calculate the distance between the two units. But due to the -1 we can attack diagonal units, but also sometimes 1 node extra on the range.
-            int nodesBetweenPlayers = GridCombatSystem.Instance.CalculatePoints(unit.GetPosition(), transform.position).Count - 1;
+		public bool IsInRange(CoreUnit unit) {
+			// Calculate the distance between the two units. But due to the -1 we can attack diagonal units, but also sometimes 1 node extra on the range.
+			int nodesBetweenPlayers = GridCombatSystem.Instance.CalculatePoints(unit.GetPosition(), transform.position).Count - 1;
 			return nodesBetweenPlayers <= range_;
-        }
+		}
 
-        public int GetCost() {
-            return cost_;
-        }
-    }
+		public int GetCost() {
+			return cost_;
+		}
+	}
 }
--- Assets/Scripts/Core/Interactables/IInteractable.cs
+++ Assets/Scripts/Core/Interactables/IInteractable.cs
@@ -1,7 +1,7 @@
 namespace OperationBlackwell.Core {
-    public interface IInteractable {
-        void Interact();
-        bool IsInRange(CoreUnit unit);
-        int GetCost();
-    }
+	public interface IInteractable {
+		void Interact();
+		bool IsInRange(CoreUnit unit);
+		int GetCost();
+	}
 }
