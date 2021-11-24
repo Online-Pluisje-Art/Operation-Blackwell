9008f1c686e1eb1f95508dd75a8b22dfc0c62e10
Noah Knegt <github@noahknegt.com>
2021-11-24 14:24:06 +0100

    player: Change weapons to have damage range and costs range
    Signed-off-by: Noah Knegt <github@noahknegt.com>

--- Assets/Scripts/Player/Weapons/Weapon.cs
+++ Assets/Scripts/Player/Weapons/Weapon.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using OperationBlackwell.Core;
 
 namespace OperationBlackwell.Player {
 	[CreateAssetMenu(fileName = "New Weapon", menuName = "Player/Weapon")]
 	public class Weapon : ScriptableObject {
-		[SerializeField] protected float damage_;
+		[SerializeField] protected List<float> damage_;
+		[SerializeField] protected List<int> costs_;
 		[SerializeField] protected float range_;
-		[SerializeField] protected int costs_;
 		[SerializeField] protected float baseHitchance_;
 		[SerializeField] protected string name_;
+		[SerializeField] protected Actions.AttackType type_;
 
 		public virtual float GetDamage() {
-			return damage_;
+			return Random.Range(damage_[0], damage_[1]);
 		}
 
 		public virtual float GetRange() {
@@ -20,7 +22,7 @@ namespace OperationBlackwell.Player {
 		}
 		
 		public virtual int GetActionPointsCost() {
-			return costs_;
+			return Random.Range(costs_[0], costs_[1]);
 		}
 
 		public virtual float GetBaseHitchance() {
@@ -29,6 +31,10 @@ namespace OperationBlackwell.Player {
 
 		public virtual string GetName() {
 			return name_;
+		}
+
+		public virtual Actions.AttackType GetAttackType() {
+			return type_;
 		}
 	}
 }
